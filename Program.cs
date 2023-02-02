9f836e056270082bf5ed49d9cd61191f8ed01589
Roman Bunyak <compcc@ya.ru>
2023-02-02 22:19:23 +0300

    create repo GitHub add .gitignore

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
